dd4923cda1ee7872cffaa5bc35a1d81dfaba9987
Oliver <oliverisler@icloud.com>
2022-11-26 15:40:41 +0100

    add solution

---
+++ Zookeeper/task/src/animal/Bat.java
@@ -0,0 +1,22 @@
+package animal;
+
+public class Bat extends Animal {
+
+    @Override
+    public void print() {
+        System.out.println("""
+                Switching on the camera in the bat habitat...
+                _________________               _________________
+                 ~-.              \\  |\\___/|  /              .-~
+                     ~-.           \\ / o o \\ /           .-~
+                        >           \\  W  //           <
+                       /             /~---~\\             \\
+                      /_            |       |            _\\
+                         ~-.        |       |        .-~
+                            ;        \\     /        i
+                           /___      /\\   /\\      ___\\
+                                ~-. /  \\_/  \\ .-~
+                                   V         V
+                This bat looks like it's doing fine.""");
+    }
+}
---
+++ Zookeeper/task/src/animal/Lion.java
@@ -0,0 +1,25 @@
+package animal;
+
+public class Lion extends Animal {
+
+    @Override
+    public void print() {
+        System.out.println("""
+                Switching on the camera in the lion habitat...
+                                                               ,w.
+                                                             ,YWMMw  ,M  ,
+                                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                                   _.-""        '''           YP"WMMMMMMMMMb,
+                                .-' __.'                   .'     MMMMW^WMMMM;
+                    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+                 ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+                ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+                WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+                "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                           /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                          /  .'             /  (       .'  /     Ww._     `.  `"
+                         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                        (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+                The lion is roaring!""");
+    }
+}
